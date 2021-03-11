0bfa6d98523c1aaf59471a9c59fc2a84acbf7abc
Linus Kemme <linus.kemme@posteo.de>
2021-03-11 22:51:27 +0100

    renamed project

--- thg-webapp/wsgi.py
+++ thg-webapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rechner.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thg-webapp.settings')
 
 application = get_wsgi_application()
