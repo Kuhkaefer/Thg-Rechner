f8c8aba43825f781434f704622210f3675899cf9
Linus Kemme <linus.kemme@posteo.de>
2021-03-11 23:46:14 +0100

    Added Question and category

--- rechner/models.py
+++ rechner/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+## Create your models here.
+
+    
+# Category
+class Category(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+# Question 
+class Question(models.Model):
+    
+    question_text = models.CharField(max_length=200)
+    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, to_field='name', default='Unsortiert') # wollen wir wirklich, dass Fragen gelöscht werden, wenn wir die Kategorie löschen?
+    
+    def __str__(self):
+        return self.question_text
+    
