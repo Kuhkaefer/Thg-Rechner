5987901fd190ea45056203c644ba2117c9f7a677
Linus Kemme <linus.kemme@posteo.de>
2021-03-13 22:50:13 +0100

    Added Template and Defaults models

--- rechner/models.py
+++ rechner/models.py
@@ -14,6 +14,16 @@ class Category(models.Model):
         return self.name
     
     
+# Product
+class Product(models.Model):
+    
+    # Name of product
+    name = models.CharField(max_length=50, unique=True)
+
+    # Representation
+    def __str__(self):
+        return self.name
+    
 # Question 
 class Question(models.Model):
     
@@ -33,11 +43,28 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
-# Product
-class Product(models.Model):
+# Templates for Event Types
+class EventTemplate(models.Model):
     
-    # Name of product
+    # Name of event type
     name = models.CharField(max_length=100, unique=True)
+
+    # Link to questions
+    questions = models.ManyToManyField(Question)
+
+    # Representation
+    def __str__(self):
+        return self.name
     
+class DefaultAmounts(models.Model):
+
+    # Link to question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     
+    # Link to template
+    template = models.ForeignKey(EventTemplate, on_delete=models.CASCADE)
+
+    # Representation
+    def __str__(self):
+        return f"default for {self.question.name} in {self.template.name}"
     
