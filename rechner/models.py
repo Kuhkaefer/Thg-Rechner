0528a1da5388c40745476680409f02bde493ed20
Linus Kemme <linus.kemme@posteo.de>
2021-03-13 21:41:48 +0100

    Calc_co2 hinzugefugt

--- rechner/models.py
+++ rechner/models.py
@@ -6,8 +6,10 @@ from django.db import models
 # Category
 class Category(models.Model):
     
+    # Name of category
     name = models.CharField(max_length=100, unique=True)
     
+    # Representation
     def __str__(self):
         return self.name
     
@@ -15,10 +17,17 @@ class Category(models.Model):
 # Question 
 class Question(models.Model):
     
+    # Text of question
     question_text = models.CharField(max_length=200)
     
+    # Link to category
     category = models.ForeignKey(Category, on_delete=models.CASCADE, to_field='name', default='Unsortiert') # wollen wir wirklich, dass Fragen gelöscht werden, wenn wir die Kategorie löschen?
     
+    # Calculation as string. Should be executable using eval(). Eg: "answer1.amount*Emission5.value"
+    calc_co2 = models.CharField(max_length=200)
+        
+    
+    # Representation
     def __str__(self):
         return self.question_text
     
