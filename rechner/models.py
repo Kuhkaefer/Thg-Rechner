02935bf093105f835d16f1600851793c110b25dc
Linus Kemme <linus.kemme@posteo.de>
2021-03-13 22:36:17 +0100

    added product model

--- rechner/models.py
+++ rechner/models.py
@@ -23,11 +23,21 @@ class Question(models.Model):
     # Link to category
     category = models.ForeignKey(Category, on_delete=models.CASCADE, to_field='name', default='Unsortiert') # wollen wir wirklich, dass Fragen gelöscht werden, wenn wir die Kategorie löschen?
     
-    # Calculation as string. Should be executable using eval(). Eg: "answer1.amount*Emission5.value"
+    # Link to products
+    products = models.ManyToManyField(Product)
+    
+    # Calculation as string. Should be executable using eval(). Eg: "self.products[0]*answer1.amount*Emission5.value"
     calc_co2 = models.CharField(max_length=200)
-        
     
     # Representation
     def __str__(self):
         return self.question_text
+    
+# Product
+class Product(models.Model):
+    
+    # Name of product
+    name = models.CharField(max_length=100, unique=True)
+    
+    
     
