d5f936c50ec7683c28e8d9bca0efa4088ac89c88
SimonJacobsen <simon.jacobsen@uol.de>
2021-04-19 17:44:37 +0200

    added new models and registered in admin

--- list/admin.py
+++ list/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Emission, Kategorie, Frage
+
+admin.site.register(Emission)
+admin.site.register(Kategorie)
+admin.site.register(Frage)
--- list/models.py
+++ list/models.py
@@ -1,7 +1,47 @@
 from django.db import models
 
 # Create your models here.
-class Emissions(models.Model):
-    name        = models.TextField()
-    description = models.TextField()
-    value       = models.TextField()
+class Emission(models.Model):
+    name = models.CharField(
+        max_length=200,
+        help_text='Füge einen Titel für die Emission hinzu'
+        )
+    beschreibung = models.TextField(
+        max_length=1000,
+        help_text='Füge eine Beschreibung der Emission hinzu'
+        )
+    wert = models.IntegerField(
+        max_length=10,
+        help_text='C02 Menge in Gramm'
+        )
+    kategorie = models.ForeignKey(
+        'kategorie',
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text='Wähle eine Kategorie für die Emission'
+        )
+
+    def __str__(self):
+        return self.name
+
+
+class Kategorie(models.Model):
+    name = models.CharField(
+        max_length=200
+        )
+
+    def __str__(self):
+        return self.name
+
+
+class Frage(models.Model):
+    name = models.CharField(
+        max_length=200,
+        help_text='Füge eine prägnante Frage hinzu (z.B.: Wie viele Flyer wurden gedruckt?)'
+        )
+    kategorie = models.ForeignKey(
+        Kategorie,
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text='Wähle eine Kategorie für die Frage'
+    )
