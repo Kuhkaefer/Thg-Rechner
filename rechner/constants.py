b814dfec39fd938508a01cb14365a90689e70373
Linus Kemme <linus.kemme@posteo.de>
2021-05-08 18:43:56 +0200

    Restructuring working partly

--- rechner/constants.py
+++ rechner/constants.py
@@ -6,4 +6,5 @@ iC = 1
 iV = 2
 iF = 3
 iL = 4
-columns = 5
+iO = 5
+columns = 6
--- rechner/views.py
+++ rechner/views.py
@@ -68,14 +68,15 @@ def result(request):
 ## Abfrage-Seite
 def fill_event_template(request, template_id):
     
-    # First Call
-    if (request.method == "GET"):
-        
-        # Initialize values
-
-        # event template
-        event_template = get_object_or_404(EventTemplate, pk=template_id) 
-        request.session['event_template_id'] = event_template.pk
+    ## Always 1 
+    
+    # event template
+    event_template = get_object_or_404(EventTemplate, pk=template_id) 
+    
+    
+    ## First Call
+    if request.method == "GET":
+        ## Initialize values
 
         # template questions list and defaults as values list
         def_amnts = event_template.defaultamounts_set.all()
@@ -84,18 +85,86 @@ def fill_event_template(request, template_id):
             data[i,C.iQ] = df.question.pk
             data[i,C.iV] = df.value
             data[i,C.iC] = df.question.category.pk
+            data[i,C.iO] = i
             
-        # sort by category
-        data = data[data[:, C.iC].argsort()]
-            
+        # sort by category and Order
+        idx = np.lexsort((data[:,C.iO],data[:,C.iC]))
+        data = data[idx]
+        
         # get first and last bools
         data = H.get_first_and_last(data)
+
+        
+    ## Later Call
+    elif request.method == "POST":
+        ## Read session
+        data = np.array(request.session['user_data'])
+        
+        ## Read user input
+        
+        # Read added fields
+        added_field_qid = max(list(map(float,request.POST.getlist("new_field"))))
+
+        # User did add field
+        if added_field_qid >= 0:    
+            print(f"field added. With question {added_field_qid}")
+            row = np.zeros(data[0].shape)
+            row[C.iQ] = added_field_qid
+            row[C.iC] = get_object_or_404(Question, pk=added_field_qid).pk
+            row[C.iO] = np.max(data[:,C.iO])+1
+            data = np.vstack([data,row])
+            
+            # Sort data again
+            idx = np.lexsort((data[:,C.iO],data[:,C.iC]))
+            data = data[idx]
+            
+            # Update first and last bools
+            data = H.get_first_and_last(data)
+            
+        
+        # Read entered values
+        for i,q in enumerate(data[:,C.iQ]):
+            data[i,C.iV] = request.POST.get(str(q.pk))
+            
+            
+        ## Process User input 
+        
+        # User pressed "submit" button
+        if request.POST.get("submit"):
+            
+            # save entered values
+            request.session['user_data'] = dataa.tolist()
+                
+            # Move on to next page
+            return HttpResponseRedirect('/rechner/result')
     
+    
+    ## Always
+    
+    # get missing qs and cats
+    missing_qs, missing_cats = H.get_missing(data)
+    
+    ## Save data
+    request.session['event_template_id'] = template_id
+    request.session['user_data'] = data.tolist()
+    
+    ## DEbug
+    
+    print(data)
+        
     
     # Create context
+    
+    q_list = []
+    for qid in data[:,C.iQ]:
+        q_list.append(get_object_or_404(Question, pk=qid))
+        print(qid)
+        print(get_object_or_404(Question, pk=qid).question_text)
+    
+    print(q_list)
     context = {
         'template_instance':event_template,
-        'q_v_f_and_l':zip(data[:,C.iQ],data[:,C.iV],data[:,C.iF],data[:,C.iL]),
+        'q_v_f_and_l':zip(q_list,data[:,C.iV],data[:,C.iF],data[:,C.iL]),
         'missing_qs':missing_qs,
         'missing_cats': missing_cats,
         'page_name':f"CO2 bei {event_template.name}",
