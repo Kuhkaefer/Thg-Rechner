d5c7ef3588761fcb337fc601f1ae0d435babd196
Linus Kemme <linus.kemme@posteo.de>
2022-09-27 11:58:48 +0200

    changed colors

--- rechner/constants.py
+++ rechner/constants.py
@@ -11,3 +11,13 @@ iU = 6 # does the question have a unit
 iI = 7 # does the question have an info text
 iO = 8 # order
 columns = 9
+
+colors = {
+    "Verpflegung":"#d62728",
+    "Mobilität":"#1f77b4",
+    "Unterkunft":"#bcbd22",
+    "Räumlichkeit":"#ff7f0e",
+    "Technik":"#9467bd",
+    "Sonstiges":"#8c564b",
+    "Materialien + Präsente":"#2ca02c",
+}
